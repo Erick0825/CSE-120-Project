64f95b9a273a805df66e64d9db1d3f71babb1db6
Erick0825 <95515038+Erick0825@users.noreply.github.com>
2022-04-14 17:25:05 -0700

    Add files via upload

--- Scan logo page.py
+++ Scan logo page.py
@@ -9,19 +9,27 @@ main.geometry("400x200")
 main['background'] = '#8a0602'
 
 
-
 main.title("Betts Company")
 
-button = tkinter.Button(main, text="Scan Logo", highlightbackground='#3E4149')
+#User is able to upload a logo
+button = tkinter.Button(main, text="Upload Logo", highlightbackground='#3E4149')
 button.grid(row=1, column=0)
 button.pack(side='bottom')
+button.place(x=0, y=0)
 
-exit = tkinter.Button(text="Would you like to return to the main page? Yes or no\n", highlightbackground='#3E4149')
-exit = exit.lower()
+
+#returns user to the main menu
+exit = tkinter.Button("Would you like to return to the main page? Yes or no\n", highlightbackground='#3E4149')
+exit = exit.mainloop()
+exit.grid(row=1, column=0)
+exit.pack(side='bottom')
+exit.place(x=0, y=0)
+
 
 if exit == "yes" or "y":
     player = False
 else:
-    main()
-mainloop()
- 
+    main() 
+    mainloop()
+    
+
