a3fc25384d1e5179f3baccb16a7457f9d64b4319
ivethurs <iaguilar6@ucmerced.edu>
2022-04-15 21:45:49 -0700

    refactored starting page to have starting buttons

--- resultDisplay.py
+++ resultDisplay.py
@@ -2,16 +2,18 @@ import tkinter
 from tkinter import *
 from turtle import exitonclick
 import vision_system
-
+root = Tk()
 class resultDisplay:
     def _init_(self):
-        main = Tk()
-        main.geometry("400x200")
-        main['background'] = '#8a0602'
-        main.title("Result")
+        
+        root.geometry("400x200")
+        root['background'] = '#8a0602'
+        root.title("Result")
 
     def display(self):
         if vision_system.match():
             label = tkinter.Label(text="Pass", background="#00FF00")
         else:
             label = tkinter.Label(text="Fail", background="#FF0000")
+            
+root.mainloop()
--- startPage.py
+++ startPage.py
@@ -13,7 +13,7 @@ set foreground: fg = ""
 """
 
 root= Tk()
-def initialWindow():
+def startWindow():
     
     root.geometry("1920x1080")
     root['background'] = '#DFDDD1'
@@ -21,16 +21,16 @@ def initialWindow():
 # set the business logo on title 
     root.iconbitmap('out.jpg')
 # def initClick(): <- how to create class to build functions for tkinter
+    msg = Label(root, text = "Hello, Welcome to Bett's Logo Detector",font=('Sans',26,BOLD))
+    msg.place(relx= 0.5, rely = 0.2, anchor= CENTER )
+    start = tk.Button(root, text="Start",font=('Sans',22,BOLD), padx= 50,pady=20)
 
-    button1 = tk.Button(root, text="Detect With This Logo",font=('Sans',22,BOLD), padx= 20,pady=20)
-# button1.grid(padx=10,pady=10)
-
-    button1.place(relx= 0.58, rely = 0.8, anchor= NW)
-
+    start.place(relx= 0.5, rely = 0.7, anchor= CENTER)
+"""
     button2 = tk.Button(root, text="Use a different Logo", font=('Sans',22,BOLD), padx= 20,pady=20)
-    button2.place(relx= 0.47, rely = 0.8, anchor= NE, )
+    button2.place(relx= 0.47, rely = 0.8, anchor= NE )"""
     
-initialWindow()
+startWindow()
 root.mainloop()
 
 
