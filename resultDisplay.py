41acda9e49b96656efa983d12fb753293451513d
David Yang <dyang57@ucmerced.edu>
2022-04-20 02:25:04 -0700

    idk i just need to pull

--- resultDisplay.py
+++ resultDisplay.py
@@ -1,11 +1,10 @@
 import tkinter
 from tkinter import *
 from turtle import exitonclick
-import vision_system
+from vision_system import vision_system
 
 class resultDisplay:
     def _init_(self):
-        main = Tk()
         main.geometry("400x200")
         main['background'] = '#8a0602'
         main.title("Result")
@@ -15,3 +14,6 @@ class resultDisplay:
             label = tkinter.Label(text="Pass", background="#00FF00")
         else:
             label = tkinter.Label(text="Fail", background="#FF0000")
+
+main = Tk()
+main.mainloop()
