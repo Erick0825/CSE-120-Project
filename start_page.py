72ce04419add0b1fb9a93055ec7ea9456bcb0ae8
ivethurs <iaguilar6@ucmerced.edu>
2022-05-01 21:50:25 -0700

    visual changes to start page

--- start_page.py
+++ start_page.py
@@ -2,7 +2,7 @@ from logging import root
 from tkinter import *
 import tkinter as tk
 from tkinter.font import BOLD
-
+from PIL import ImageTk, Image
 from setuptools import Command
 
 
@@ -20,19 +20,26 @@ def go_to_next():
 root = Tk()
 
 
-def startwindow():
+def startwindow():   
     root.geometry("1920x1080")
     root['background'] = '#DFDDD1'
+    
     root.title("Welcome Page")
     # set the business logo on title
     root.iconbitmap('out.jpg')
     # def initClick(): <- how to create class to build functions for tkinter
-    msg = Label(root, text="Hello, Welcome to Bett's Logo Detector", font=('Sans', 26, BOLD))
+    msg = Label(root, text="Hello, Welcome to Bett's Logo Detector!", font=('Sans', 26, BOLD),fg='black')
+    msg.config(bg= '#DFDDD1')
     msg.place(relx=0.5, rely=0.2, anchor=CENTER)
 
-    start = tk.Button(root, text="Start", font=('Sans', 22, BOLD), padx=50, pady=20, command=go_to_next)
+    start = tk.Button(root, text="Start", font=('Sans', 26, BOLD), padx=50, pady=20, command=go_to_next)
 
     start.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+img=PhotoImage(file='betts.png')
+img_place = Label(root,image=img,)
+img_place.config(bg= '#DFDDD1')
+img_place.place(relx=0.5, rely=0.9, anchor=CENTER)
 
 
 startwindow()
