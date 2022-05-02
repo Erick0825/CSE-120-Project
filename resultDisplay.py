106d7e02f18382da97019de201c9ad83096585f3
ivethurs <iaguilar6@ucmerced.edu>
2022-05-01 23:36:49 -0700

    result visual edits

--- resultDisplay.py
+++ resultDisplay.py
@@ -5,15 +5,16 @@ from tkinter.font import BOLD
 from setuptools import Command
 # import vision_system
 
-win = Tk()
+
 # command to go to camera page
 def toCam():
     win.quit()
 
 # command to go to start page
 def toStart():
-    win.quit()
-
+    win.destroy()
+    import start_page
+win = Tk()
 # window created
 def result():
     # window will display fail if vision_system passes "fail" result
@@ -22,7 +23,7 @@ def result():
         # red
         win['background'] = '#FF0000'
         win.title("Result")
-        msg = Label(win, text="Fail", font=('Sans', 72, BOLD))
+        msg = Label(win, text="Fail", font=('Sans', 72, BOLD),bg= '#FF0000')
         msg.place(relx=0.5, rely=0.4, anchor=CENTER)
         # camera page button
         camera = tk.Button(win, text="Camera", font=('Sans', 22, BOLD), padx=50, pady=20, command=toCam)
@@ -32,11 +33,11 @@ def result():
         start.place(relx=0.7, rely=0.7, anchor=CENTER)
     # window will display pass if vision_system passes "pass" result
     else:
-        win.geometry("700x350")
+        win.geometry("1000x900")
         # green
         win['background'] = '#00FF00'
         win.title("Result")
-        msg = Label(win, text="Pass", font=('Sans', 72, BOLD))
+        msg = Label(win, text="Pass", font=('Sans', 72, BOLD), bg= '#00FF00')
         msg.place(relx=0.5, rely=0.4, anchor=CENTER)
 
         camera = tk.Button(win, text="Scan", font=('Sans', 22, BOLD), padx=50, pady=20, command=toCam)
