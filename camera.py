6b362574edb369a4ab654dc378f6388b3a0ce0fc
ivethurs <iaguilar6@ucmerced.edu>
2022-05-03 19:58:20 -0700

    new window that display images that have been submitted

--- camera.py
+++ camera.py
@@ -10,21 +10,23 @@ win = Tk()
 win.geometry("700x350")
 
 # Create a Label to capture the Video frames
-label =Label(win)
+label = Label(win)
 label.grid(row=0, column=0)
-cap= cv2.VideoCapture(0)
+cap = cv2.VideoCapture()
+
 
 # Define function to show frame
 def show_frames():
-   # Get the latest frame and convert into Image
-   cv2image= cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2RGB)
-   img = Image.fromarray(cv2image)
-   # Convert image to PhotoImage
-   imgtk = ImageTk.PhotoImage(image = img)
-   label.imgtk = imgtk
-   label.configure(image=imgtk)
-   # Repeat after an interval to capture continiously
-   label.after(20, show_frames)
+    # Get the latest frame and convert into Image
+    cv2image = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(cv2image)
+    # Convert image to PhotoImage
+    imgtk = ImageTk.PhotoImage(image=img)
+    label.imgtk = imgtk
+    label.configure(image=imgtk)
+    # Repeat after an interval to capture continiously
+    label.after(20, show_frames)
+
 
 show_frames()
 win.mainloop()
---
+++ didsplay_submit.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from tkinter.filedialog import askopenfile
+from PIL import Image, ImageTk
+import confirm
+my_w = Tk()
+my_w.title("Betts Company")
+my_w.geometry("1000x900")
+my_w['background'] = '#DFDDD1'
+my_font1=('times', 18, 'bold')
+l1 = tk.Label(my_w,text='Upload Files & display',width=30,font=my_font1)  
+l1.grid(row=1,column=1,columnspan=4)
+b1 = tk.Button(my_w, text='Upload Files',font=('Sans', 18, 'bold'), padx=10, pady=10, 
+command = lambda:upload_file())
+b1.place(relx=0.4, rely=0.95, anchor=CENTER)
+
+def nextPage():
+        my_w.destroy()
+        confirm.confirmpage(vision, f_types)
+
+
+NextPage = tk.Button(
+    my_w, 
+    text="Next Page", font=('Sans', 18, 'bold'), padx=10, pady=10,
+    
+    command=nextPage
+    )
+NextPage.place(relx=0.6, rely=0.95, anchor=CENTER)
+
+def upload_file():
+    
+    f_types = [('Jpg Files', '*.jpg'),('PNG Files','*.png'), ('Jpeg Files','*.jpeg')]
+    filename = tk.filedialog.askopenfilename(multiple=True,filetypes=f_types)
+    col=1 # start from column 1
+    row=3 # start from row 3 
+    for f in filename:
+        img=Image.open(f) # read the image file
+        img=img.resize((200,200)) # new width & height
+        img=ImageTk.PhotoImage(img)
+        e1 =tk.Label(my_w)
+        e1.grid(row=row,column=col)
+        e1.image = img
+        e1['image']=img # garbage collection 
+        if(col==3): # start new line after third column
+            row=row+1# start wtih next row
+            col=1    # start with first column
+        else:       # within the same row 
+            col=col+1 # increase to next column       
+def nextPage():
+    my_w.destroy()
+    confirm.confirmpage(vision, f_types)          
+my_w.mainloop()  # Keep the window open
